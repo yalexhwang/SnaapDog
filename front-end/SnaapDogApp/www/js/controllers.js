76689f681d0ebe87a35385b0f36a18c3264f472f
DIWithers <d.ivy.withers@gmail.com>
2016-10-19 11:30:52 -0400

    MANUAL MERGE

--- front-end/SnaapDogApp/www/js/controllers.js
+++ front-end/SnaapDogApp/www/js/controllers.js
@@ -1,90 +1,326 @@
 angular.module('starter.controllers', [])
 
-
-
-
-.controller("PictureCtrl", function($scope, $cordovaCamera) {
-    document.addEventListener("deviceready", function () {
-         $scope.takePicture = function() {
-        var options = { 
-            quality : 75, 
-            destinationType : Camera.DestinationType.DATA_URL, 
-            sourceType : Camera.PictureSourceType.CAMERA, 
-            allowEdit : true,
-            encodingType: Camera.EncodingType.JPEG,
-            targetWidth: 300,
-            targetHeight: 300,
-            popoverOptions: CameraPopoverOptions,
-            saveToPhotoAlbum: false
-        };
- 
-        $cordovaCamera.getPicture(options).then(function(imageData) {
-            $scope.imgURI = "data:image/jpeg;base64," + imageData;
-        }, function(err) {
-            // An error occured. Show a message to the user
-        });
-    }
-
-    })
+.service('myLocation', function($q, $http, $window, $cordovaGeolocation) {
+  this.locate = function() {
+    var def = $q.defer();
+    var coords = {};
+    // var posOptions = {
+    //   enableHighAccuracy: true,
+    //   timeout: 2000,
+    //   maximumAge: 0
+    // };
+    var posOptions = {timeout: 10000, enableHighAccuracy: false};
+    $cordovaGeolocation.getCurrentPosition(posOptions)
+    .then(function success(position) {
+      console.log(position);
+      def.resolve(position);
+    }, function fail(rspns) {
+      console.log(rspns);
+      def.reject(rspns);
+    });
+    return def.promise;
+  }
 })
 
-.controller('ListingsCtrl', function($scope, $ionicModal) {
-  $scope.message = "works";
-  $scope.data = {
-    "filter": "lost",
-      "strays": [
-    {
-      photo: "img/stray1.jpeg",
-      size: 'large',
-      color: 'brown/black',
-      status: 'lost'
-    },
-     {
-      photo: "img/stray2.jpeg",
-      size: 'large',
-      color: 'brown/white',
-      status: 'found'
-    },
-     {
-      photo: "img/stray3.jpeg",
-      size: 'large',
-      color: 'tan',
-      status: 'found'
-    },
-     {
-      photo: "img/stray4.jpeg",
-      size: 'small',
-      color: 'brown/black',
-      status: 'found'
-    },
-     {
-      photo: "img/stray5.jpeg",
-      size: 'large',
-      color: 'grey',
-      status: 'lost'
-    },
-     {
-      photo: "img/stray6.jpeg",
-      size: 'large',
-      color: 'white/black',
-      status: 'lost'
+.service('fromDB', function($http, $q) {
+  // var data = {};
+  var url = "http://localhost:3000";
+  this.strayEntries = function() {
+    var def = $q.defer();
+    $http.post(url + '/stray_entries')
+    .then(function success(rspns) {
+      console.log(rspns);
+      def.resolve(rspns);
+
+    }, function fail(rspns) {
+      def.reject(rspns);
+    });
+    return def.promise;
+  };
+})
+
+//Controllers - - - - - - - - - - - - -
+
+.controller('DashCtrl', function($scope, $ionicSideMenuDelegate) {
+  console.log('hello tab dash');
+})
+
+.controller('LogInCtrl', function($scope, $http, $stateParams, $state) {
+  var url = "http://localhost:3000";
+  $scope.user = {};
+  $scope.login = function() {
+    $http.post(url + '/login', {
+      user: $scope.user
+    }).then(function success(rspns) {
+      $rootScope.user = rspns.data.docs;
+      console.log($rootScope.user);
+  $state.go('tab.dash');  //where there is main, put dash
+    }, function sutccess(rspns) {
+      console.log(rspns);
+    });
+  };
+})
+
+
+.controller('RegisterCtrl', function($scope, $http, $stateParams, $state) {
+  $scope.home = function(){
+    $state.go('tabs.dash', {});
+  };
+  $scope.landing = function(){
+    $state.go('landing', {});
+  };
+  var url = "http://localhost:3000";
+  $scope.user = {};
+  $scope.confirm = {};
+  var pw = $scope.confrimPW;
+  var temp = {
+    fname: 'Alex',
+    lname: 'Hwang',
+    username: 'ah',
+    email: 'ah@test.com',
+    password: 111
+  };
+
+  $scope.register = function() {
+    console.log($scope.test);
+    console.log($scope.user);
+    console.log($scope.confirm);
+    if (temp.password == 111) {
+      console.log("Password match");
+      $http.post(url + '/register', {
+        user: temp
+      }).then(function success(rspns) {
+        console.log(rspns);
+        $location.path('/logIn');
+      }, function fail(rspns) { 
+        console.log("error")
+      });
+    } else {
+      console.log("Try confirmPW again");
     }
-    ] //end strays object
+  };
+})
+
+.controller('TabsCtrl', function($scope, $ionicModal, $timeout, $ionicSideMenuDelegate) {
+  $scope.openMenuLeft = function() {
+    $ionicSideMenuDelegate.toggleLeft();
+  };
+  })
+
+
+.controller('SomethingCtrl', function($scope, $http) {
+  console.log("something")
+})
+
+
+.controller('mainCtrl', function($scope, $rootScope, $cordovaGeolocation, $ionicLoading, $ionicPlatform, fromDB, myLocation) {
+  $ionicPlatform.ready(function() {
+    $ionicLoading.show({
+      template: '<ion-spinner icon="bubbles"></ion-spinner><br/>Finding your location'
+    });
+    // $ionicLoading.hide();
+  });
+  var markers = [];
+  var myLoc = {};
+  var map; 
+
+  fromDB.strayEntries().then(function success(rspns) {
+    var entries = rspns.data.docs;
+    $rootScope.stray_entries = entries;
+    $scope.totalEntries = $rootScope.stray_entries.length;
+    console.log($rootScope.stray_entries);
+  }, function fail(rspns) {
+    console.log(rspns);
+  })
+  .then(function success(rspns) {
+    myLocation.locate().then(function success(rspns) {
+      var lat = rspns.coords.latitude;
+      var lng = rspns.coords.longitude;
+      myLoc = new google.maps.LatLng(lat, lng);
+      map = new google.maps.Map(document.getElementById('map'), {
+        center: myLoc,
+        zoom: 8
+      });
+      $ionicLoading.hide();
+    }, function fail(rspns) {
+      console.log(rspns);
+    })
+    .then(function success(rspns) {
+      for (var i = 0; i < $rootScope.stray_entries.length; i++) {
+        placeMarkers($rootScope.stray_entries[i], map);
+      }
+    }, function fail(rspns) {
+      console.log(rspns);
+    });
+  }, function fail(rspns) {
+    console.log(rspns);
+  });
+  
+  var infoWindow = new google.maps.InfoWindow({});
+  function placeMarkers(entry, map) {
+    var icon = 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=•%7CFE7569';
+    var content = '<h4>' + entry.dog_name + '</h4><p>' + entry.description + '</p>' + '<a ng-click="streetView()">Here</a>';
+    var marker = new google.maps.Marker({
+        position: {lat: entry.location.coords.lat, lng: entry.location.coords.lng},
+        icon: icon,
+        map: map,
+        title: entry.dog_name,
+        animation: google.maps.Animation.DROP
+      });
+    marker.addListener('click', function() {
+      infoWindow.setContent(content);
+      infoWindow.open(map, marker);
+      map.setZoom(11);
+      map.setCenter(marker.getPosition());
+    });
+    markers.push(marker);
   }
 
-  $scope.options = {
-    loop: false,
-    effect: 'fade',
-    speed: 500
-  }
-//SORT LIST
-$scope.sortType = 'status'
-$scope.sortReverse = false;
-$scope.searchPosts = "";
-//SORT LIST END
+})
+.controller('snaapCtrl', function($scope, $cordovaCamera, $ionicPlatform) {
+  $scope.test ="works";
+  $scope.takePhoto = function () {
+    var options = {
+      quality: 75,
+      destinationType: Camera.DestinationType.DATA_URL,
+      sourceType: Camera.PictureSourceType.CAMERA,
+      allowEdit: true,
+      encodingType: Camera.EncodingType.JPEG,
+      targetWidth: 300,
+      targetHeight: 300,
+      popoverOptions: CameraPopoverOptions,
+      saveToPhotoAlbum: false
+    };
+   $cordovaCamera.getPicture(options).then(function(imageData) {
+      $scope.imgURI = "data:image/jpeg;base64," + imageData;
+    }, function (err) {
+      // An error occured. Show a message to the user
+    });
+  };
+                
+  $scope.choosePhoto = function() {
+    var options = {
+      quality: 75,
+      destinationType: Camera.DestinationType.DATA_URL,
+      sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+      allowEdit: true,
+      encodingType: Camera.EncodingType.JPEG,
+      targetWidth: 300,
+      targetHeight: 300,
+      popoverOptions: CameraPopoverOptions,
+      saveToPhotoAlbum: false
+    };
+    $cordovaCamera.getPicture(options).then(function(imageData) {
+       $scope.imgURI = "data:image/jpeg;base64," + imageData;
+    }, function (err) {
+      // An error occured. Show a message to the user
+    });
+  };
+                
+})
 
+.controller('postCtrl', function($scope, $rootScope, $http, myLocation) {
+  var url = "http://localhost:3000";
+  console.log('postCtrl');
+  $http.post(url + '/post_categories')
+  .then(function success(rspns) {
+    console.log(rspns);
+    var breeds = rspns.data.docs.breeds;
+    var sizes = rspns.data.docs.sizes;
+    var coats = rspns.data.docs.coats;
+    var colors = rspns.data.docs.colors;
+    $scope.breedArr = [];
+    for (var prop in breeds[0]) {
+      var item = [prop, breeds[0][prop]];
+      $scope.breedArr.push(item);
+    }
+    $scope.sizeArr = [];
+    for (var prop in sizes[0]) {
+      var item = [prop, sizes[0][prop]];
+      $scope.sizeArr.push(item);
+    }
+    $scope.coatArr = [];
+    for (var prop in coats[0]) {
+      var item = [prop, coats[0][prop]];
+      $scope.coatArr.push(item);
+    }
+    $scope.colorArr = [];
+    for (var prop in colors[0]) {
+      var item = [prop, colors[0][prop]];
+      $scope.colorArr.push(item);
+    }
+  }, function fail(rspns) {
+    console.log(rspns);
+  });
 
- //MODAL
+  myLocation.locate().then(function success(rspns) {
+    var lat = rspns.coords.latitude;
+    var lng = rspns.coords.longitude;
+    myLoc = new google.maps.LatLng(lat, lng);
+    console.log(myLoc);
+    var map = new google.maps.Map(document.getElementById('map-sm'), {
+      center: myLoc,
+      zoom: 13
+    });
+    google.maps.event.addListener(map, 'click', function(event) {
+      console.log(event.latLng);
+      var marker = new google.maps.Marker({
+        position: event.latLng
+      });
+      console.log(marker.getPosition());
+      dropAPin(marker, map);
+    });
+
+    var markerArr = [];
+    function dropAPin(marker, map) {
+      for (var i = 0; i < markerArr.length; i++) {
+        markerArr[i].setMap(null);
+      }
+      markerArr = [];
+      markerArr.push(marker);
+      for (var i = 0; i < markerArr.length; i++) {
+        markerArr[i].setMap(map);
+        map.panTo(location);
+      }
+    }
+  }, function fail(rspns) {
+
+  });
+  $scope.post = {};
+  $scope.owner = $rootScope.user.username;
+  $scope.submit = function() {
+    console.log($scope.post);
+    if (($scope.post.owner == undefined) || ($scope.post.owner == "")) {
+      $scope.post.owner = $scope.owner;
+    }
+    var user = {
+      id: $rootScope.user._id,
+      username: $rootScope.user.username
+    };
+    console.log(user);
+   $http.post(url + '/post', {
+      user: user,
+      post: $scope.post
+    }).then(function succeess(rspns) {
+      console.log(rspns);
+
+    }, function fail(rspns) {
+
+    });
+  };
+})
+
+.controller('listingCtrl', function($scope, $http) {
+  var url = "http://localhost:3000";
+  $http.post(url + '/listing')
+  .then(function succeess(rspns) {
+    console.log(rspns.data);
+    $scope.starys = rspns.docs;
+  }, function fail(rspns) {
+    console.log(rspns);
+  });
+  console.log("listings");
+  //MODAL
   $ionicModal.fromTemplateUrl('templates/modal.html', {
     scope: $scope, animation: 'slide-in-up'
   }).then(function(modal) {
@@ -111,65 +347,21 @@ $scope.searchPosts = "";
     // Execute action
   });
 //MODAL END
-//---- UNCOMMENT THIS AFTER TEST!
-// .controller('listingCtrl', function($scope, $http) {
-//  var url = "http://localhost:3000";
-//  $http.post(url + '/listing')
-//  .then(function succeess(rspns) {
-//    console.log(rspns.data);
-//    $scope.strays = rspns.docs;
-//  }, function fail(rspns) {
-//    console.log(rspns);
-//  });
-//  console.log("listings");
-// })
-//----
 
 }) //end listing ctrl
 
-.controller('DashCtrl', function($scope) {
 
-
-}) // end dashctrl
-
-.controller('LogInCtrl', function($scope, $http, $stateParams) {
-
-  })
-
-.controller('RegisterCtrl', function($scope, $http, $stateParams) {
-
-  })
-
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
   $scope.chat = Chats.get($stateParams.chatId);
 })
 
-.controller('AccountCtrl', function($scope) {
+.controller('AccountCtrl', function($scope, $state) {
   $scope.settings = {
     enableFriends: true
   };
+  $scope.home = function(){
+    $state.go('tabs.dash', {});
+  };
 });
-
-
-
-
-
-
-
-
 
--- readme.md
+++ readme.md
@@ -2,9 +2,85 @@
 
 ##SnaapDog enables an open collaboration between seekers, finders, and shelters to get roaming dogs home
 
+![Alt text](img/ScreenShot2.png "Home Page")
+
+
 ### Users will be able to:
 * Create an alert for their lost pet and notify others in a set radius - tagged with description key words
 * Upload a picture of a roaming dog and insert into the database as a sighting - tagged with key words
 * Connect with each other without revealing personal contact information
 
+Visit here: [Portfolio](http://shirletterly.com/)
+
 ###Requirements `npm install` once pulled
+
+
+##Built with:
+	- Html
+	- CSS
+	- Javascript
+	- jQuery 
+	- Bootstrap
+
+##Sample Code
+### this code...
+
+```javascript
+.controller('mainCtrl', function($scope, $rootScope, $cordovaGeolocation, $ionicLoading, $ionicPlatform, fromDB, myLocation) {
+  $ionicPlatform.ready(function() {
+    $ionicLoading.show({
+      template: '<ion-spinner icon="bubbles"></ion-spinner><br/>Finding your location'
+    });
+    // $ionicLoading.hide();
+  });
+  var markers = [];
+  var myLoc = {};
+  var map; 
+
+  fromDB.strayEntries().then(function success(rspns) {
+    var entries = rspns.data.docs;
+    $rootScope.stray_entries = entries;
+    $scope.totalEntries = $rootScope.stray_entries.length;
+    console.log($rootScope.stray_entries);
+  }, function fail(rspns) {
+    console.log(rspns);
+  })
+  .then(function success(rspns) {
+    myLocation.locate().then(function success(rspns) {
+      var lat = rspns.coords.latitude;
+      var lng = rspns.coords.longitude;
+      myLoc = new google.maps.LatLng(lat, lng);
+      map = new google.maps.Map(document.getElementById('map'), {
+        center: myLoc,
+        zoom: 8
+      });
+      $ionicLoading.hide();
+    }, function fail(rspns) {
+      console.log(rspns);
+    })
+    .then(function success(rspns) {
+      for (var i = 0; i < $rootScope.stray_entries.length; i++) {
+        placeMarkers($rootScope.stray_entries[i], map);
+      }
+    }, function fail(rspns) {
+      console.log(rspns);u
+    });
+  }, function fail(rspns) {
+    console.log(rspns);
+  });
+```
+
+
+####Early stages of site
+![Alt text](img/ScreenShot1.png "Early stages of site")
+
+<!-- add a video of interaction with the site -->
+
+##Future Add-ons
+- Links to more current projects.
+- Ability for visitors to send messages directly from the site to me.
+- An enhanced UX/UI design that encourages more visitors.
+
+
+###Please visit my personal profile to see more current projects.
+- [Shirlette Chambers](https://github.com/Shirlazybrat)
